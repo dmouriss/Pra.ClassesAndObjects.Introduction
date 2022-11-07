75904a37d209d8a67e7d31282738c2f1c8c3576e
Dieter Mourisse <dieter.mourisse@howest.e>
2022-11-07 15:32:23 +0100

    Add project files.

---
+++ Pra.ClassesAndObjects.Introduction.Core/BankAccount.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pra.ClassesAndObjects.Introduction.Core
+{
+    public class BankAccount
+    {
+
+        public Person owner;
+        public string accountNumber;
+        public decimal balance;
+
+        public BankAccount(Person owner, string accountNumber)
+        {
+            this.owner = owner;
+            this.balance = 0;
+            this.accountNumber = accountNumber;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount > balance)
+                throw new ArgumentException("Insufficient funds");
+            balance -= amount;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            balance += amount;
+        }
+
+        public void Transfer(BankAccount recepient, decimal amount)
+        {
+            Withdraw(amount);
+            recepient.Deposit(amount);
+        }
+
+        public override string ToString()
+        {
+            return accountNumber;
+        }
+
+    }
+}
---
+++ Pra.ClassesAndObjects.Introduction.Core/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pra.ClassesAndObjects.Introduction.Core
+{
+    public class Person
+    {
+        public string firstname;
+        public string lastname;
+
+        public Person(string firstname, string lastname)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+        }
+
+        public override string ToString()
+        {
+            return $"{firstname} {lastname}";
+        }
+    }
+}
